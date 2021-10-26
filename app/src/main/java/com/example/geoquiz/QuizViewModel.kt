07af238229adbaf7ad3cec321616049e603a12d9
shahad197 <shahad.yousef.jed.k2@tuwaiq.edu.sa>
2021-10-27 00:07:44 +0300

    ....

---
+++ app/src/main/java/com/example/geoquiz/QuizViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.geoquiz
+
+import android.nfc.Tag
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+private const val Tag = "QuizViewModel"
+class QuizViewModel:ViewModel() {
+
+
+
+     val questionBank = listOf(
+        Question(R.string.first_question,true,"shahad"),
+        Question(R.string.second_question,false,"anas"),
+        Question(R.string.third_question,false,"ahmed")
+    )
+        var currentIndex = 0
+    val currentQuestion = 0
+        val currentQuestionText : Int
+                get()=questionBank[currentIndex].textResId
+        val currentuestionAnswer : Boolean
+                get() = questionBank[currentIndex].answer
+
+        val instructorName :String
+        get()=questionBank[currentIndex].instructor
+
+        fun nextQuestion(){
+            currentIndex = (currentIndex+1) % questionBank .size
+
+        }
+
+
+
+}

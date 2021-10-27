0bf3bebb4915ffe57911a573645328a2f5627baf
roya allam <Roya.Adel.jed.k2@tuwaiq.edu.sa>
2021-10-27 08:42:27 +0300

    thank you

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
    private lateinit var falseButton:Button
    private lateinit var trueButton:Button
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
